b2e18c05d75eef73ec31428fbba11a67f07f3977
MikoZ321 <0123456789miko@gmail.com>
2022-04-02 14:52:49 +0200

    Finish game logic

--- script.js
+++ script.js
@@ -24,13 +24,37 @@ function playRound (playerSelection) {
     playerSelection = playerSelection.toLowerCase();
     switch (true) {
         case (playerSelection === 'rock'): {
-            
+            if (computerSelection === 'paper') {
+                result = 'Lose';
+            }
+            else if (computerSelection === 'scissors') {
+                result = 'Win';
+            }
+            else {
+                result = 'Tie';
+            }
         }
         case (playerSelection === 'paper'): {
-
+            if (computerSelection === 'scissors') {
+                result = 'Lose';
+            }
+            else if (computerSelection === 'rock') {
+                result = 'Win';
+            }
+            else {
+                result = 'Tie';
+            }
         }
         case (playerSelection === 'scissors'): {
-
+            if (computerSelection === 'rock') {
+                result = 'Lose';
+            }
+            else if (computerSelection === 'paper') {
+                result = 'Win';
+            }
+            else {
+                result = 'Tie';
+            }
         }
         default: {
             throw new ReferenceError('playerSelection is not a valid argument');
