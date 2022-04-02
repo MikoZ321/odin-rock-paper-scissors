0fd8fde89820011c7f319216d22cedcadea90b39
MikoZ321 <0123456789miko@gmail.com>
2022-04-02 14:05:26 +0200

    Add function to generate a move

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const random = n => Math.floor(Math.random() * n)
 
 function computerPlay () {
-    
+    let n = random(3)
+    let move;
+    if (n === 0) {
+        move = "rock";
+    }
+    else if (n === 1) {
+        move = "paper";
+    }
+    else {
+        move = "scissors";
+    }
+    return move;
 }
