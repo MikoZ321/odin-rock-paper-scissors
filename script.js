74df21757b3acf3d91d10c2930cd85fbed0eb729
MikoZ321 <0123456789miko@gmail.com>
2022-04-02 19:17:59 +0200

    Add result indicator

--- script.js
+++ script.js
@@ -15,8 +15,7 @@ function computerPlay () {
     return move;
 }
 
-function playRound (playerSelection) {
-    let computerSelection = computerPlay();
+function getOutcome (playerSelection, computerSelection) {
     let result;
     if (playerSelection === undefined) {
         throw new ReferenceError('playerSelection is not defined');
@@ -26,38 +25,61 @@ function playRound (playerSelection) {
         case (playerSelection === 'rock'): {
             if (computerSelection === 'paper') {
                 result = 'Lose';
+                return result;
             }
             else if (computerSelection === 'scissors') {
                 result = 'Win';
+                return result;
             }
             else {
                 result = 'Tie';
+                return result;
             }
         }
         case (playerSelection === 'paper'): {
             if (computerSelection === 'scissors') {
                 result = 'Lose';
+                return result;
             }
             else if (computerSelection === 'rock') {
                 result = 'Win';
+                return result;
             }
             else {
                 result = 'Tie';
+                return result;
             }
         }
         case (playerSelection === 'scissors'): {
             if (computerSelection === 'rock') {
                 result = 'Lose';
+                return result;
             }
             else if (computerSelection === 'paper') {
                 result = 'Win';
+                return result;
             }
             else {
                 result = 'Tie';
+                return result;
             }
         }
         default: {
             throw new ReferenceError('playerSelection is not a valid argument');
         }
+    }
+}
+
+function playRound (playerSelection) {
+    let computerSelection = computerPlay();
+    let result = getOutcome(playerSelection, computerSelection);
+    if (result === 'Win') {
+        console.log(`You win, ${playerSelection} beats ${computerSelection}!`);
+    }
+    else if (result === 'Lose') {
+        console.log(`You lose, ${computerSelection} beats ${playerSelection}.`)
+    }
+    else {
+        console.log(`You tie, ${computerSelection} ties with ${playerSelection}`)
     }
 }
